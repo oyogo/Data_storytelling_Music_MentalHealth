e49e85918ce9b097fa60f699b55d98f022023c81
oyogo <oyogoc@gmail.com>
2023-07-18 09:43:44 +0300

    python approach.

---
+++ pyway.py
@@ -0,0 +1,13 @@
+# Understanding the relationship between music and mental health
+# Let's explore the data from JB Music
+# import libraries
+import pandas as pd
+
+pydata = pd.read_csv("data/Music_and_Mental_Health_-_Survey.csv")
+
+# Display the first five records to have an idea of how the data looks like
+pydata.head()
+
+# Tree map 
+# Tree maps can be used to visualize hierachical data. 
+
